3db48c936923f561d13d4377f9b7fa3df9911443
Venus Velazquez <venus.dv10@gmail.com>
2023-10-26 20:09:11 -0400

    initial file setup

--- project5.py
+++ project5.py
@@ -1,5 +1,19 @@
-#!/usr/bin/env python3.2
+#!/usr/bin/env python3
+#   Author:		Venus Velazquez
+#	Major:		CS
+#	Due Date:	Oct. 31, 2023
+#	Course:		CSC328 020
+#	Assignment:	Socket programming
+#	Filename:	project5.py
+#	Purpose: 	This client program reads word data from a server
 
-bytes = b'\x00\x05'
-integer = int.from_bytes(bytes, byteorder= 'big')
-print(integer)
+import struct
+
+# Description: 
+# Parameters:  
+# Returns:     
+
+
+
+if __name__ == "__main__":
+    print(integer)
